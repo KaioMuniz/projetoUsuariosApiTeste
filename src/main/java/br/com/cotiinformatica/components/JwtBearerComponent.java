0a184a039ea7d91e6ff843608e1ada0e61fe8127
KaioMuniz <kkaioribeiro@gmail.com>
2025-06-24 11:23:49 -0300

    metodos gettoken e getexpiration

--- src/main/java/br/com/cotiinformatica/components/JwtBearerComponent.java
+++ src/main/java/br/com/cotiinformatica/components/JwtBearerComponent.java
@@ -5,6 +5,9 @@ import java.util.Date;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+
 @Component
 public class JwtBearerComponent {
 
@@ -26,14 +29,21 @@ public class JwtBearerComponent {
 	 * Método para calcular e retornar a data de expiração do token
 	 */
 	public Date getExpiration() {
-		return null; //TODO
+		//Data atual + o tempo de expiração em milisegundos
+		var dataAtual = new Date();
+		return new Date(dataAtual.getTime() + Integer.parseInt(jwtExpiration));
 	}
 	
 	/*
 	 * Método para gerar e retornar o Token do usuário autenticado
 	 */
 	public String getToken(String emailUsuario) {
-		return null; //TODO
-	}
-	
+		
+		return Jwts.builder()
+				.setSubject(emailUsuario) //identificação do usuário do token
+				.setIssuedAt(new Date()) //data de geração do token
+				.setExpiration(getExpiration()) //data de expiração do token
+				.signWith(SignatureAlgorithm.HS256, jwtSecret) //chave de assinatura
+				.compact(); //finaliza e retorna o token gerado
+	}	
 }
